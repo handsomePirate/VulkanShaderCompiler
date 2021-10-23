e9e48aefc70d4309c4cff5d6d98b2914e9691f33
Vojtěch Kužel <adalbert.kuzel@seznam.cz>
2021-10-23 13:26:34 +0200

    Modified logger so that it can be overriden from the outside

--- include/VulkanShaderCompiler/Logger.hpp
+++ include/VulkanShaderCompiler/Logger.hpp
@@ -1,5 +1,10 @@
 #pragma once
+
+#ifndef CompilerLogger
+
 #include <SoftwareCore/Singleton.hpp>
 #include <SoftwareCore/Logger.hpp>
 
 #define CompilerLogger ::Core::Singleton<::Core::Logger>::GetInstance()
+
+#endif
